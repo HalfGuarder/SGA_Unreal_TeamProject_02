ef48a0c96ab5909df89c2341e23d07988dc956c8
ddochi-bit <swo12345@naver.com>
2024-11-14 11:49:27 +0900

    Rampage_DeathAnimAdd_collisionAdd

--- TFT_Project_B/Source/TFT_Project_B/TFT_Characters/TFT_BossMonster_Rampage.cpp
+++ TFT_Project_B/Source/TFT_Project_B/TFT_Characters/TFT_BossMonster_Rampage.cpp
@@ -49,6 +49,9 @@ void ATFT_BossMonster_Rampage::BeginPlay()
 
     _statCom->SetLevelAndInit(1);
     PlayerController = GetWorld()->GetFirstPlayerController();
+
+    armcapsule_R->OnComponentBeginOverlap.AddDynamic(this, &ATFT_BossMonster_Rampage::OnArmCapsuleHit);
+    armcapsule_L->OnComponentBeginOverlap.AddDynamic(this, &ATFT_BossMonster_Rampage::OnArmCapsuleHit);
 }
 
 void ATFT_BossMonster_Rampage::PostInitializeComponents()
@@ -94,27 +97,6 @@ void ATFT_BossMonster_Rampage::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    /*if (PlayerController && HpBarWidgetComponent)
-    {
-        
-        APawn* PlayerPawn = PlayerController->GetPawn();
-        if (PlayerPawn)
-        {
-            
-            float DistanceToPlayer = FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
-
-            
-            if (DistanceToPlayer <= HpBarVisibilityDistance)
-            {
-                HpBarWidgetComponent->SetVisibility(true);
-            }
-            else
-            {
-                HpBarWidgetComponent->SetVisibility(false);
-            }
-        }
-    }*/
-
     AActor* Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
     if (Player)
@@ -158,7 +140,7 @@ void ATFT_BossMonster_Rampage::AttackHit_Boss()
         GetActorLocation(),
         GetActorLocation() + GetActorForwardVector() * attackRange,
         FQuat::Identity,
-        ECollisionChannel::ECC_GameTraceChannel10,
+        ECollisionChannel::ECC_GameTraceChannel6,
         FCollisionShape::MakeSphere(attackRadius),
         params
     );
@@ -203,8 +185,53 @@ void ATFT_BossMonster_Rampage::AttackHit_Boss()
         }
     }
 
+    armcapsule_R->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    armcapsule_L->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
     DrawDebugSphere(GetWorld(), center, attackRadius, 20, drawColor, false, 2.0f);
 }
+
+void ATFT_BossMonster_Rampage::OnArmCapsuleHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (OtherActor && OtherActor != this && _isAttacking)
+    {
+        // 타겟이 공격 가능한 타입인지 확인
+        ATFT_Creature* Target = Cast<ATFT_Creature>(OtherActor);
+        if (Target)
+        {
+           
+            float hpRatio = _statCom->BossHPRatio();
+            float damageMultiplier = (hpRatio < 0.3f) ? 2.0f : 1.0f;
+            float baseDamage = _statCom->GetAttackDamage();
+            float damage = baseDamage * damageMultiplier;
+
+            FDamageEvent DamageEvent;
+            Target->TakeDamage(damage, DamageEvent, GetController(), this);
+
+           
+            switch (_curAttackIndex)
+            {
+            case 1:
+                Target->SetState(StateType::Airborne);
+                break;
+            case 2:
+                Target->SetState(StateType::Stun);
+                break;
+            case 3:
+                Target->SetState(StateType::Slow);
+                break;
+            default:
+                break;
+            }
+
+           
+            armcapsule_R->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+            armcapsule_L->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        }
+    }
+}
+
+
 
 void ATFT_BossMonster_Rampage::Attack_AI()
 {
@@ -270,6 +297,8 @@ void ATFT_BossMonster_Rampage::ResetMovementLock(UAnimMontage* Montage, bool bIn
 {
     _isAttacking = false;
 
+    armcapsule_R->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    armcapsule_L->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
     _animInstance_Boss->OnMontageEnded.RemoveDynamic(this, &ATFT_BossMonster_Rampage::ResetMovementLock);
 }
--- TFT_Project_B/Source/TFT_Project_B/TFT_Characters/TFT_BossMonster_Rampage.h
+++ TFT_Project_B/Source/TFT_Project_B/TFT_Characters/TFT_BossMonster_Rampage.h
@@ -23,7 +23,10 @@ public:
 
 	UFUNCTION()
 	void AttackHit_Boss();
-	
+
+	UFUNCTION()
+	void OnArmCapsuleHit(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 	virtual void Attack_AI();
 	void ResetSkillCooldown();
 	
